7f0f8c78932b7fe9d596771f2e5cb6b5f8605a98
rlama7 <royalray7@gmail.com>
2019-08-06 20:11:32 -0700

    Updated doubly linked list insertAt() method.

--- LinkedList/src/com/ratnalama/DoublyLinkedList.java
+++ LinkedList/src/com/ratnalama/DoublyLinkedList.java
@@ -76,11 +76,77 @@ public class DoublyLinkedList {
         if (head.getNext() == null) {
             head = null;
         } else {
-            // more than a single node present in the chain
-            last.getPrevious().setNext(null); // the last node's previous node's next field will point to null
+            // more than a single node present in
+            // the last node's previous node's next field will point to null
         }
         last = last.getPrevious();
         return temp;
     } // end deleteLast()
+
+    // insertAfter() -inserts a node after a particular key
+    public boolean insertAfter(int key, int data) {
+        Node current = head;    // start from the beginning of the list
+        // iterate through the chain until we find the key
+        while (current.getData() != key) {
+            current = current.getNext();
+            // we reached the end of the loop and couldn't find the key
+            if (current == null) {
+                return false;
+            }
+        }
+        Node newNode = new Node();
+        newNode.setData(data);
+
+        // if the node is the last one in the LinkedList chain then update the last node
+        if (current == last) {
+            current.setNext(null);
+            last = newNode;
+        } else {
+            // some where in the middle of the Linked List chain
+            // newNode's next field should point to the old current note's next field
+            newNode.setNext(current.getNext());
+            // old current's next node's previous field should now point to the newNode.
+            current.getNext().setPrevious(newNode);
+        }
+        // newNode's previous should point to the current
+        newNode.setPrevious(current);
+        current.setNext(newNode);
+        return true;    // if we've made this far then we successfully made the insertion after a key
+    } // end insertAfter()
+
+    // deleteKey() - deletes a node with a particular key data
+    public Node deleteKey(int key) {
+        Node current = head;    // start from the beginning of the LinkedList chain
+        Node tempNode;
+        // iterate through the chain until we find the node with key data to be deleted
+        while (current.getData() != key) {
+            current = current.getNext();    // advance to the next node
+            // we've reached the end of the linkedList chain and we didn't find the key
+            if (current == null) {
+                return null;
+            }
+        }
+        /**
+         * At this stage we've found the Node with matching key
+          */
+
+        // check if the Node is the first Node
+        if (current == head) {
+            head = current.getNext();   // link the head to the next node of the current's node
+        } else {
+            current.getPrevious().setNext(current.getNext());
+        }
+
+        // check if the Node is the last Node
+        if (current == last) {
+            last = current.getPrevious();
+        } else {
+            current.getNext().setPrevious(current.getPrevious());
+        }
+        return current;
+    } // end deleteKey()
+
+    // displayForward()
+    // displayBackward()
 
 } // end DoublyLinkedList Class
